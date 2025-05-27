e467e74a84436a78c02a96861c2585acf7535744
PaulZC <pc235603@gmail.com>
2025-05-27 12:00:14 +0100

    Adding .a

--- examples/Example01_BasicUsage/Example01_BasicUsage.ino
+++ examples/Example01_BasicUsage/Example01_BasicUsage.ino
@@ -8,14 +8,80 @@ void setup()
 
     // Start serial
     Serial.begin(115200);
-    Serial.println("Authentication Coprocessor - Basic Uasge");
+    Serial.println("Authentication Coprocessor - Basic Usage");
 
+    // Start I2C
     Wire.begin();
-
-    myDevice.begin();
-
 }
 
 void loop()
 {
+    // Check the device is connected - and awake
+    myDevice.begin(Wire);
+
+    // Read the certificate serial number
+    char certSerial[32 + 1]; // 32 ASCII characters plus NULL terminator
+    if (myDevice.getDeviceCertificateSerialNumber(sizeof(certSerial), (uint8_t *)certSerial))
+    {
+        Serial.print("Certificate Serial Number: ");
+        Serial.println(certSerial);
+    }
+
+    // Read the certificate
+    uint8_t cert[609]; // Cert could be up to 609 bytes
+    size_t certSize;
+    if (myDevice.getDeviceCertificate(sizeof(cert), cert, certSize))
+    {
+        Serial.print("Certificate Size: ");
+        Serial.println(certSize);
+
+        printHexThing("Certificate", certSize, cert);
+    }
+
+    // Write the challenge data
+    uint8_t challengeData[32] =
+    {
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+        0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F
+    };
+    if (myDevice.setChallengeData(challengeData, sizeof(challengeData)))
+        Serial.println("Challenge accepted");
+
+    // Start the challenge response generation
+    if (myDevice.startChallenge())
+        Serial.println("Challenge response generation started");
+
+    delay(500); // Wait tAUTH
+
+    // Read the challenge response
+    uint8_t challengeResponse[64];
+    if (myDevice.getChallengeResponse(sizeof(challengeResponse), challengeResponse))
+        printHexThing("Challenge Response", sizeof(challengeResponse), challengeResponse);
+
+    // Repeat after 5 seconds - coprocessor will go to sleep after 4 seconds
+    delay(5000);
 }
+
+void printHexThing(const char *name, size_t thingSize, uint8_t *thing)
+{
+    Serial.print(name);
+    Serial.print(":\r\n{");
+    for (size_t x = 0 ; x < thingSize ; x++)
+    {
+        const size_t rowLength = 32;
+        if (x % rowLength == 0)
+            Serial.println();
+        //Serial.printf(" 0x%02X,", thing[thingSize - x - 1]); //Print in reverse order
+        Serial.printf("0x%02X", thing[x]); //Normal order
+        if (x < (thingSize - 1))
+        {
+            Serial.print(",");
+            if ((x % rowLength) < (rowLength - 1))
+                Serial.print(" ");
+        }
+    }
+    Serial.println("\r\n}");
+}
+
--- src/SparkFun_Auth_3_CP.h
+++ src/SparkFun_Auth_3_CP.h
@@ -13,9 +13,6 @@ class SparkFunAuth3CPDriver
         /**
          * @brief Initializes communication with the Auth 3.0 Coprocessor.
          *
-         * @param wirePort The I2C port to communicate with the coprocessor.
-         *                 Defaults to `Wire`.
-         *
          * @return Returns `true` if the initialization was successful, `false` otherwise.
          *
          * @note Call this method before using any other functions in the class.
@@ -29,25 +26,21 @@ class SparkFunAuth3CPDriver
          * @brief Read the certificate serial number.
          *
          * @param maxLen The maximum length of the certificate serial number.
-         *               I.e. sizeof certSerial
+         *               I.e. sizeof(certSerial)
          *
          * @param certSerial Pointer to the certificate serial number storage.
          *                   If the read is successful, this will contain the
-         *                   certificate serial number.
-         *
-         * @param certSerialLen Pointer to the certificate serial number length.
-         *                      If the read is successful, this will contain the
-         *                      actual certificate serial number length.
+         *                   certificate serial number (ASCII).
          *
          * @return Returns `true` if the read was successful, `false` otherwise.
          *
          */
-        bool getDeviceCertificateSerialNumber(uint16_t maxLeng, uint8_t *certSerial, uint16_t &certSerialLen);
+        bool getDeviceCertificateSerialNumber(size_t maxLen, uint8_t *certSerial);
 
         /**
          * @brief Read the certificate.
          *
-         * @param maxLen The maximum length of the certificate. I.e. sizeof cert.
+         * @param maxLen The maximum length of the certificate. I.e. sizeof(cert).
          *
          * @param cert Pointer to the certificate storage. If the read is successful,
          *             this will contain the certificate.
@@ -58,42 +51,35 @@ class SparkFunAuth3CPDriver
          * @return Returns `true` if the read was successful, `false` otherwise.
          *
          */
-        bool getDeviceCertificate(uint16_t maxLeng, uint8_t *cert, uint16_t &certLen);
+        bool getDeviceCertificate(size_t maxLen, uint8_t *cert, size_t &certLen);
 
         /**
          * @brief Set (write) the challenge data.
          *
-         * @param maxLen The maximum length of the certificate. I.e. sizeof cert.
-         *
          * @param challenge Pointer to the challenge data.
          *
-         * @param challengeLen The length of the challenge data. I.e. sizeof challenge.
+         * @param challengeLen The length of the challenge data. I.e. sizeof(challenge).
          *
          * @return Returns `true` if the set was successful, `false` otherwise.
          *
          */
-        bool setChallengeData(uint8_t *challenge, uint16_t challengeLen);
+        bool setChallengeData(uint8_t *challenge, size_t challengeLen);
 
         /**
          * @brief Start the challenge response generation.
          *
          */
-        void startChallenge();
-
+        bool startChallenge(void);
 
         /**
          * @brief Read the challenge response.
          *
          * @param maxLen The maximum length of the response.
-         *               I.e. sizeof response
+         *               I.e. sizeof(response)
          *
          * @param response Pointer to the challenge response storage.
          *                 If the response generation was successful,
          *                 this will contain the challenge response.
-         *
-         * @param responseLen Pointer to the challenge response length.
-         *                    If the response generation was  successful,
-         *                    this will contain the actual response length.
          *
          * @return Returns `true` if both the response generation and the
          *         read were successful, `false` otherwise.
@@ -101,8 +87,8 @@ class SparkFunAuth3CPDriver
          * @note Response generation takes 500ms maximum. Wait 500 millis
          *       between startChallenge and getChallengeResponse.
          */
-        bool getChallengeResponse(uint16_t maxLeng, uint8_t *response, uint16_t &responseLen);
-
+        bool getChallengeResponse(size_t maxLen, uint8_t *response);
+    
     protected:
         /// @brief Sets the communication bus to the specified bus.
         /// @param theBus Bus to set as the communication device.
@@ -118,7 +104,7 @@ class SparkFunAuth3CPArdI2C : public SparkFunAuth3CPDriver
   public:
     SparkFunAuth3CPArdI2C() {}
 
-    /// @brief  Sets up Arduino I2C driver using the default I2C address then calls the super class begin.
+    /// @brief  Sets up Arduino I2C driver using the default I2C address and port, then calls the super class begin.
     /// @return True if successful, false otherwise.
     bool begin(void)
     {
@@ -132,7 +118,8 @@ class SparkFunAuth3CPArdI2C : public SparkFunAuth3CPDriver
         return SparkFunAuth3CPDriver::begin();
     }
 
-    /// @brief  Sets up Arduino I2C driver using the specified I2C address then calls the super class begin.
+    /// @brief  Sets up Arduino I2C driver using the specified port, then calls the super class begin.
+    /// @param wirePort The I2C port to communicate with the coprocessor.
     /// @return True if successful, false otherwise.
     bool begin(TwoWire &wirePort)
     {
@@ -141,7 +128,7 @@ class SparkFunAuth3CPArdI2C : public SparkFunAuth3CPDriver
 
         setCommunicationBus(&_theI2CBus);
 
-        _theI2CBus.setStop(false); // Use restarts not stops for I2C reads
+        _theI2CBus.setStop(true); // Use stops not restarts for I2C reads
 
         return SparkFunAuth3CPDriver::begin();
     }
